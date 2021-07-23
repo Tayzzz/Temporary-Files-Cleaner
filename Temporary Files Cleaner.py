3b8de90ffac5a4eb02003e2e05b4c3444119b331
Tayzzz <42284450+Tayzzz@users.noreply.github.com>
2021-07-23 20:33:07 +0200

    Update Temporary Files Cleaner.py

--- Temporary Files Cleaner.py
+++ Temporary Files Cleaner.py
@@ -1,19 +1,16 @@
-try:
-    import colorama
-    import shutil
-except ModuleNotFoundError as e:
-    modulename = str(e).split("No module named ")[1].replace("'", "")
-    input(f"Please install module with: pip install {modulename}")
-    exit()
-
 import os
 import shutil
+import ctypes
 from colorama import Fore, init
 
 init(convert=True)
 
 
 def main():
+
+    ctypes.windll.kernel32.SetConsoleTitleW(
+        "Temporary Files Cleaner | v0.1 | By Tayz")
+
     folders = [os.environ.get('TEMP').replace("\\", "/"),
                os.environ.get("USERPROFILE").replace("\\", "/") + "/Downloads"]
 
@@ -23,9 +20,9 @@ def main():
     for folder in folders:
 
         choice = input(
-            f"Do you want clear files and folders in {folder} (yes/no) ? ")
+            f"Do you want clear files and folders in {folder} (y/n) ? ")
 
-        if str(choice) == "yes":
+        if str(choice) == "yes" or str(choice) == "y":
 
             if not os.listdir(folder):
                 print(f'{Fore.RED}No folders/files{Fore.RESET}\n')
@@ -51,9 +48,10 @@ def main():
 
                     except Exception as e:
                         print(f'{Fore.RED}Access Denied{Fore.RESET} {itemName}')
+
                 print()
         else:
-            pass
+            print()
 
     input(str(deleteFileCount) + ' files and ' +
           str(deleteFolderCount) + ' folders deleted. Press enter to exit')
